95f9eb22d0c53c4007641734c77e5ba3a4370d95
afqaffendi <202348758@student.uitm.edu.my>
2025-01-02 21:48:32 +0800

    Adding Google Authentication

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,11 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
+    apiKey: 'AIzaSyBrBRKaUIH2leDmb3mMgqjUMaYZeB5RtkM',
+    authDomain: 'friendlychat-35639.firebaseapp.com',
+    projectId: 'friendlychat-35639',
+    storageBucket: 'friendlychat-35639.firebasestorage.app',
+    messagingSenderId: '998415873771',
+    appId: '1:998415873771:web:95a251a72381354ff370ae',
+    measurementId: 'G-KT9DYWMCYX',
   },
 };
---
+++ src/firebase-config.js
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase: {
+      apiKey: 'AIzaSyBrBRKaUIH2leDmb3mMgqjUMaYZeB5RtkM',
+      authDomain: 'friendlychat-35639.firebaseapp.com',
+      projectId: 'friendlychat-35639',
+      storageBucket: 'friendlychat-35639.firebasestorage.app',
+      messagingSenderId: '998415873771',
+      appId: '1:998415873771:web:95a251a72381354ff370ae',
+      measurementId: 'G-KT9DYWMCYX',
+    },
+  };
